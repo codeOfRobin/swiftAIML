bfa0446758eb286ecf4644a2de190b1cd7548712
Robin Malhotra <malhotra.robin@icloud.com>
2016-05-15 19:31:55 +0530

    Initial Commit

---
+++ swiftAIML/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  swiftAIML
+//
+//  Created by Robin Malhotra on 15/05/16.
+//  Copyright © 2016 Robin Malhotra. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ swiftAIML/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  swiftAIML
+//
+//  Created by Robin Malhotra on 15/05/16.
+//  Copyright © 2016 Robin Malhotra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
