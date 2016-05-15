1cb0a817420714b307a2d555454257f35ce18602
Robin Malhotra <malhotra.robin@icloud.com>
2016-05-16 01:03:27 +0530

    init
    OH GOD CORE PLOT IS HORRIBLE

--- swiftAIML/ViewController.swift
+++ swiftAIML/ViewController.swift
@@ -7,17 +7,51 @@
 //
 
 import Cocoa
+import CorePlot
+class ViewController: NSViewController,CPTPlotDataSource
+{
 
-class ViewController: NSViewController {
-
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var plotView: CPTGraphHostingView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let graph = CPTXYGraph(frame: CGRectZero)
+        graph.title = "Hello Graph"
+        graph.paddingLeft = 0
+        graph.paddingTop = 0
+        graph.paddingRight = 0
+        graph.paddingBottom = 0
+        // hide the axes
+        let axes = graph.axisSet as? CPTXYAxisSet
+        let lineStyle = CPTMutableLineStyle()
+        lineStyle.lineWidth = 0
+        axes?.xAxis?.axisLineStyle = lineStyle
+        axes?.yAxis?.axisLineStyle = lineStyle
+        
+        // add a pie plot
+        let pie = CPTPieChart()
+        pie.dataSource = self
+        pie.pieRadius = (self.view.frame.size.width * 0.9)/2
+        graph.addPlot(pie)
+        
+        
+        self.plotView.hostedGraph = graph
     }
 
-    override var representedObject: AnyObject? {
-        didSet {
+    func numberOfRecordsForPlot(plot: CPTPlot) -> UInt
+    {
+        return 4
+    }
+    
+    func numberForPlot(plot: CPTPlot, field fieldEnum: UInt, recordIndex idx: UInt) -> AnyObject? {
+        return idx + 1
+    }
+    override var representedObject: AnyObject?
+    {
+        didSet
+        {
         // Update the view, if already loaded.
         }
     }
